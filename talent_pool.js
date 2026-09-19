0672a34f91a50f8a422702cbeb8f760db1181217
saiganeshganti <saiganeshganti@gmail.com>
2026-09-19 14:36:43 +0530

    Update talent_pool.js

--- talent_pool.js
+++ talent_pool.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://sai-ai-recruiter-backend.onrender.com";
+const API_BASE_URL = "https://in-sai.onrender.com";
 
 /* =========================================================
    TALENTREACH — TALENT POOL
